cf9868b18e9ed12be5a3fdc25f6f1d9d88627610
finley <573108440@qq.com>
2017-12-05 22:31:35 +0800

    enum

---
+++ src/main/java/com/finley/enumration/State.java
@@ -0,0 +1,7 @@
+package com.finley.enumration;
+
+enum State {
+    RECHARGING,
+    SUCCESS,
+    FAIL
+}
---
+++ src/main/java/com/finley/enumration/Test.java
@@ -0,0 +1,9 @@
+package com.finley.enumration;
+
+public class Test{
+
+    public static void main(String []args){
+        System.out.println(State.RECHARGING.ordinal()==1);
+    }
+
+}
