f8084ae9db03bb5583823a88a7a341b9af2a8d41
[finley] <[finley_1994@163.com]>
2018-01-11 19:23:51 +0800

    spring

---
+++ src/main/java/com/finley/annotation/TestTarget.java
@@ -0,0 +1,13 @@
+package com.finley.annotation;
+
+/**
+ * @author zyf
+ * @date 2017/12/14
+ */
+public class TestTarget {
+    @Server(value = false,url = "////")
+    public static void main(String []args){
+
+    }
+
+}
